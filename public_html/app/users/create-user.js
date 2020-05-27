46151a211b6f6f2a92cc065021007a0316ff346f
MariusAndro <46905578+MariusAndro@users.noreply.github.com>
2020-05-27 11:08:27 +0300

    Added files

---
+++ public_html/app/users/create-user.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+    
+   //show html form when 'create user' button is clicked
+   $(document).on('click', '.create-user-button', function(){       
+       //load list of companies
+       $.getJSON("http://localhost:8080/NetBeans/Moodle/RestApiPhpTestApp/api/company/readCompany.php", function(data){
+           //build company option html
+           //loop through returned list of data
+           
+           var companies_option_html = `<select name='company_id' class= 'form-control'>`;
+           $.each(data.records, function(key, val){
+               
+              companies_option_html+=`<option value='` + val["Company_Name"] + `'>` + val["Company_Name"] + `</option>`;
+           });           
+           companies_option_html+=`</select>`;
+           
+           
+           var create_user_html=`
+            <!-- 'create user' html form -->
+            <form id='create-user-form' action='#' method='post' border='0'>
+                <table class='table table-hover table-responsive table-bordered'>
+                    <tr>
+                        <td>Personal ID Number</td>
+                        <td><input type='text' name='personal_id_number' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>First Name</td>
+                        <td><input type='text' name='First_name' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Last Name</td>
+                        <td><input type='text' name='Last_name' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input type='email' name='Email' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type='password' name='Password' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Country</td>
+                        <td><input type='text' name='Country' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>City</td>
+                        <td><input type='text' name='City' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Street</td>
+                        <td><input type='text' name='Street' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Number</td>
+                        <td><input type='text' name='Number' class='form-control' required /></td>
+                    </tr>
+                    <tr>
+                        <td>Company</td>
+                        <td>` + companies_option_html + `</td>
+                    </tr>
+                    <tr>
+                        <td>Rank</td>
+                        <td>
+                            <select name='Rank' class='form-control'>
+                                <option value="Administrator">Administrator</option>
+                                <option value="User">User</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <button type='submit' class='btn btn-primary'>
+                                <span class='glyphicon glyphicon-plus'></span> Create User
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+             </form>`;
+       
+            //inject html to 'page-content' of our app
+            $("#page-content").html(create_user_html);            
+            //change page title
+            changePageTitle("Create User");
+                        
+        });
+       
+        //wil run if create product form was submitted                
+                $(document).on('submit','#create-user-form',function(){
+                 //console.log($(this));
+                    function getFormData($form){
+                        var unindexed_array = $form.serializeArray();
+                        var indexed_array = {};
+
+                        $.map(unindexed_array, function(n, i){
+                            indexed_array[n['name']] = n['value'];
+                        });
+
+                        return indexed_array;
+                    }
+                 
+                 var $form = $("#create-user-form");
+                 var form_data=JSON.stringify(getFormData($form));
+//                 //console.log(form_data);
+                 
+                   //submit data to api                   
+                $.ajax({
+                    url:"http://localhost:8080/NetBeans/Moodle/RestApiPhpTestApp/api/user/create.php",
+                    type : "POST",
+                    contentType : 'application/json; charset=utf-8',
+                    data : form_data,
+                    datatype: 'json',
+                    success : function(result){
+                        //user was created, go back to users list
+                        showUsers();                        
+                    },
+                    error : function(xhr, resp, text){
+                        //show error to console
+                        //console.log(xhr,resp,text);
+                    }       
+                });
+                return false;
+                });
+                
+   });      
+   
+   
+});
---
+++ public_html/app/users/read-users.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    //show users at first load    
+    showUsers();
+});
+
+$(document).on('click', 'read-users-button',function(){
+    showUsers();
+});
+
+function showUsers(){
+   //get users from API         
+   $.getJSON("http://localhost:8080/NetBeans/Moodle/RestApiPhpTestApp/api/user/readUser.php", function(data){       
+       var read_users_html=`
+            <!--when clicked, it will load the create product form' -->
+               <div id='create-user-button' class='btn btn-primary pull-right m-b-15px create-user-button'>
+                    <span class='glyphicon glyphicon-plus'></span>Create User</div>
+             
+                    <table class='table table-bordered table-hover'>
+                    <tr>
+                        <th class='w-10-pct'>User ID</th>
+                        <th class='w-25-pct'>Personal ID Number</th>
+                        <th class='w-25-pct'>First Name</th>
+                        <th class='w-25-pct'>Last Name</th>
+                        <th class='w-25-pct'>Email</th>
+                        <th class='w-25-pct'>Country</th>
+                        <th class='w-25-pct'>City</th>
+                        <th class='w-25-pct'>Street</th>
+                        <th class='w-10-pct'>Number</th>
+                        <th class='w-25-pct text-align-center'>Action</th>
+                    </tr>`;       
+        //loop through returned list of data
+        $.each(data.records, function(key, val){
+            //create new table row per record            
+            read_users_html+=`<tr>            
+                <td>` + val["User_ID"] + `</td>
+                <td>` + val["personal_id_number"] + `</td>
+                <td>` + val["First_name"] + `</td>
+                <td>` + val["Last_name"] + `</td>
+                <td>` + val["Email"] + `</td>
+                <td>` + val["Country"] + `</td>
+                <td>` + val["City"] + `</td>
+                <td>` + val["Street"] + `</td>
+                <td>` + val["Number"] + `</td>
+            
+                <!--'action' buttons -->               
+                <td style='white-space: nowrap'>
+                    <!-- read user button -->
+                    <button class='btn btn-primary m-r-10px read-one-user-button' data-id='` + val["User_ID"] + `'>
+                        <span class='glyphicon glyphicon-eye-open'></span> Read
+                    </button>
+
+                    <!-- edit button -->
+                    <button class='btn btn-info m-r-10px update-user-button' data-id='` + val["User_ID"] + `'>
+                        <span class='glyphicon glyphicon-edit'></span> Edit
+                    </button>
+
+                    <!-- delete button -->
+                    <button class='btn btn-danger delete-user-button' data-id='` + val["User_ID"] + `'>
+                        <span class='glyphicon glyphicon-remove'></span> Delete
+                    </button>
+                </td>
+            </tr>`;
+            
+        });
+       
+       //end table
+       read_users_html+=`</table>`;       
+       $("#page-content").html(read_users_html);
+       
+       changePageTitle("Read Users");
+       
+        });
+};
+
